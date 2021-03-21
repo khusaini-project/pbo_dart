a18cc0098a7ba94a858a2f6113382a663b823ff2
khusaini <@muhammadkhusaini599>
2021-03-21 23:19:13 +0700

    latihan pbo

---
+++ pbo_dart_4b/bin/latihan_pbo.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  List<int> mylist = [];
+  List<int> list = [1, 2, 3];
+
+  // for(int bilangan in list){
+  //   print(bilangan);
+  // }
+
+  list.forEach((bilangan) {
+    print(bilangan);
+  });
+}
