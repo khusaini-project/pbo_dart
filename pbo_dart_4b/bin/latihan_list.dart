364a80dec00b4fac2bceae207061ef6f34f9f358
khusaini <@muhammadkhusaini599>
2021-03-21 23:17:04 +0700

    latihan list

---
+++ pbo_dart_4b/bin/latihan_list.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  List<int> mylist = [7, 1, 6, 4, 3, 6, 2];
+  List<int> list = [];
+
+  // Set<int> s;
+  // s = mylist.toSet();
+
+  mylist.map((number) => "angka" + number.toString());
+
+  list.forEach((str) {
+    print(str);
+  });
+}
